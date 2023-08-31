9af158989d8808f9ef0492e16c2ee458a02dc11c
Balaji <balaji0017@gmail.com>
2023-08-31 11:39:38 +0530

    Added Invalid Login Test

--- OpenEMRAutomation/LoginTest.cs
+++ OpenEMRAutomation/LoginTest.cs
@@ -26,5 +26,23 @@ namespace Unisys.OpenEMRAutomation
             //wait for page load to complete 
             Assert.That(driver.Title, Is.EqualTo(expectedTitle));
         }
+
+        [Test]
+        [TestCase("saul", "saul123", "Danish", "Invalid username or password")]
+        [TestCase("kim", "kim123", "Dutch", "Invalid username or password")]
+        public void InvalidLoginTest(string username, string password, string language, string expectedError)
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.Id("clearPass")).SendKeys(password);
+            SelectElement selectLanguage = new SelectElement(driver.FindElement(By.XPath("//select[@name='languageChoice']")));
+            selectLanguage.SelectByText(language);
+            driver.FindElement(By.Id("login-button")).Click();
+
+            string actualError = driver.FindElement(By.XPath("//p[contains(text(),'Invalid')]")).Text;
+
+            Assert.That(actualError, Does.Contain(expectedError));
+        }
+
+
     }
 }
